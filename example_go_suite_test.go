4c8e25b7324434f86fd205dbddb5369256f750b3
Michael Grosser <michael@grosser.it>
2018-06-03 18:26:27 -0700

    dep

--- example_go_suite_test.go
+++ example_go_suite_test.go
@@ -3,13 +3,10 @@ package example
 import (
     . "github.com/onsi/ginkgo"
     . "github.com/onsi/gomega"
-    "os"
     "testing"
-    "github.com/onsi/ginkgo/reporters"
 )
 
 func TestAwesome(t *testing.T) {
     RegisterFailHandler(Fail)
-    junitReporter := reporters.NewJUnitReporter(os.Getenv("CI_REPORT"))
-    RunSpecsWithDefaultAndCustomReporters(t, "Awesome Suite", []Reporter{junitReporter})
+    RunSpecs(t, "Shopping Cart Suite")
 }
