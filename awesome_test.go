d9a860a2761ae408ee04374f438f6ac7d619fcd1
Michael Grosser <michael@grosser.it>
2018-06-03 18:47:23 -0700

    more

--- awesome_test.go
+++ awesome_test.go
@@ -3,20 +3,13 @@ package example
 import (
     . "github.com/onsi/ginkgo"
     . "github.com/onsi/gomega"
-    "testing"
 )
-
-func TestAwesome(t *testing.T) {
-    RegisterFailHandler(Fail)
-    RunSpecs(t, "Example")
-}
 
 var _ = Describe("Example", func() {
     Describe("Make an awesome expression", func() {
         Context("smile", func() {
             It("should result :)", func() {
-                Setup()
-                Expect(GetResult()).To(Equal(":)"))
+                Expect(":)").To(Equal(":)"))
             })
         })
     })
